3c344cfabfa40020696735827a088bead6855253
iamdeepti <iamdeepti956@gmail.com>
2019-04-18 23:14:14 +0530

    add some functions in models.py

--- djangoapp/src/mysite/polls/models.py
+++ djangoapp/src/mysite/polls/models.py
@@ -5,11 +5,20 @@ class Question(models.Model):
 	"""docstring for Questions"models.modelf __init__(self, arg):
 		super(Questions,models.model.__init__()
 		self.arg = arg"""
+
 	question_text = models.CharField(max_length = 200)
 	pub_date = models.DateTimeField('date published')
+	def __str__(self):
+		return self.question_text
 
 class Choice(models.Model):
 
 	question = models.ForeignKey(Question, on_delete = models.CASCADE)	
 	choice_text = models.CharField(max_length = 200)
 	votes = models.IntegerField(default = 0)
+	def __str__(self):
+		return self.choice_text
+
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now()-datetime.timedelta(days=1)
+	
