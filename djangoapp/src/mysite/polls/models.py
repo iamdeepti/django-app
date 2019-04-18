6e4b516a96971568acc9250751ff0f2d81ea624d
iamdeepti <iamdeepti956@gmail.com>
2019-04-18 22:43:57 +0530

    Migrate changes made in models.py

--- djangoapp/src/mysite/polls/models.py
+++ djangoapp/src/mysite/polls/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+	"""docstring for Questions"models.modelf __init__(self, arg):
+		super(Questions,models.model.__init__()
+		self.arg = arg"""
+	question_text = models.CharField(max_length = 200)
+	pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+
+	question = models.ForeignKey(Question, on_delete = models.CASCADE)	
+	choice_text = models.CharField(max_length = 200)
+	votes = models.IntegerField(default = 0)
