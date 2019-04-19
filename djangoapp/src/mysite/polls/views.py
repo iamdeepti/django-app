2c7a16cb23c0fdf0e54ec4b3dbf6e333fd317811
iamdeepti <iamdeepti956@gmail.com>
2019-04-19 14:07:02 +0530

    Add html template
    Use function loader.get_template()

--- djangoapp/src/mysite/polls/views.py
+++ djangoapp/src/mysite/polls/views.py
@@ -1,8 +1,18 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 def index(request):
-	return HttpResponse("Hello world! You're at poll index")
 
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	output = ', '.join([q.question_text for q in latest_question_list])
+	template = loader.get_template('polls/index.html')
+	context = {
+		'latest_question_list':latest_question_list,
+	}
+	return HttpResponse(template.render(context,request))
+		
 def details(request, question_id):
 	return HttpResponse("You're looking at question %s" % question_id)
 
@@ -13,11 +23,11 @@ def results(request, question_id):
 def vote(request, question_id):
 	return HttpResponse("You're voting on question %s" % question_id)
 '''def details(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+response = "You're looking at the results of question %s."
+return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)'''
+return HttpResponse("You're voting on question %s." % question_id)'''
