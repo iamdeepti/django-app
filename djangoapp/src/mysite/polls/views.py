6ef9dbcb172fa510f6c272a5d53f788e590c6d0e
iamdeepti <iamdeepti956@gmail.com>
2019-04-19 12:04:56 +0530

    modify views

--- djangoapp/src/mysite/polls/views.py
+++ djangoapp/src/mysite/polls/views.py
@@ -3,12 +3,21 @@ from django.http import HttpResponse
 def index(request):
 	return HttpResponse("Hello world! You're at poll index")
 
-def details(request,question_id):
-	return HttpResponse("You're looking at question %s" %question_id)
+def details(request, question_id):
+	return HttpResponse("You're looking at question %s" % question_id)
 
-def results(request,question_id):
+def results(request, question_id):
 	response = "You're looking at the result of question %s"
-	return(response %question_id)
+	return HttpResponse(response % question_id)
 
-def vote(request,question_id):
-	return("You're voting on question %s" %question_id)
+def vote(request, question_id):
+	return HttpResponse("You're voting on question %s" % question_id)
+'''def details(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)'''
